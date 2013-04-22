8f23f8845fda24133a847baf7831b47ce6914d8f
Faisal Mansoor <faisal.mansoor@gmail.com>
2013-04-22 11:50:53 -0400

    loaded

---
+++ EnumSwitch/EnumSwitch.cs
@@ -0,0 +1,96 @@
+﻿namespace EnumSwitch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EnumSwitch<T> where T : struct
+    {
+        private readonly Dictionary<T, Action> _caseMap = new Dictionary<T, Action>();
+        private readonly T? _switchOn;
+
+        public EnumSwitch()
+        {
+            Type enumType = typeof(T);
+            if (!enumType.IsEnum)
+            {
+                string msg = String.Format("Type argument {0} must be an enum", enumType);
+                throw new ArgumentException(msg);
+            }
+        }
+
+        public EnumSwitch(T switchOn)
+            : this()
+        {
+            _switchOn = switchOn;
+        }
+
+        public EnumSwitch<T> Case(T option, Action action)
+        {
+            if (_caseMap.Keys.Contains(option))
+            {
+                throw new ArgumentException(String.Format("Duplicate case label {0}", option), "option");
+            }
+
+            _caseMap.Add(option, action);
+
+            return this;
+        }
+
+        public EnumSwitch<T> Case(IEnumerable<T> options, Action action)
+        {
+            foreach (var item in options)
+            {
+                Case(item, action);
+            }
+
+            return this;
+        }
+
+        public EnumSwitch<T> Ignore(params T[] options)
+        {
+            return Case(options, () => { });
+        }
+
+        public EnumSwitch<T> Ignore(T option)
+        {
+            return Case(option, () => { });
+        }
+
+        public void Execute()
+        {
+            if (!_switchOn.HasValue)
+            {
+                throw new InvalidOperationException("SwitchOn not provided, either provide SwitchOn in constructor or use Execute(switchOn)");
+            }
+
+            Execute(_switchOn.Value);
+        }
+
+        public void Execute(T switchOn)
+        {
+            Validate();
+
+            Action action;
+            if (_caseMap.TryGetValue(switchOn, out action))
+            {
+                action();
+            }
+            else
+            {
+                throw new NotImplementedException(String.Format("Case {0} is not handled by this switch statement", switchOn));
+            }
+        }
+
+        private void Validate()
+        {
+            var possibleValues = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+            var missingValues = possibleValues.Except(_caseMap.Keys);
+            if (missingValues.Count() != 0)
+            {
+                string msg = "Switch statement didn't handle cases: " + String.Join(", ", missingValues);
+                throw new NotImplementedException(msg);
+            }
+        }
+    }
+}
---
+++ EnumSwitch/EnumSwitchTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EnumSwitch
+{
+    public enum Colors
+    {
+        Red,
+        Blue,
+        Green
+    }
+
+    [TestFixture]
+    public class EnumSwitchTests
+    {
+        [Test]
+        public void Case()
+        {
+            new EnumSwitch<Colors>(Colors.Red)
+                .Case(Colors.Blue, () => Console.WriteLine("Blue"))
+                .Case(Colors.Red, () => Console.WriteLine("Red"))
+                .Case(Colors.Green, () => Console.WriteLine("Green"))
+                .Execute();
+        }
+
+        [Test]
+        public void Cases()
+        {
+            new EnumSwitch<Colors>(Colors.Red)
+                .Case(new[] { Colors.Red, Colors.Blue }, () => Console.WriteLine("Red or Blue"))
+                .Case(Colors.Green, () => Console.WriteLine("Green"))
+                .Execute();
+        }
+
+        [Test]
+        public void ExecuteMultipleTimes()
+        {
+            var eSwitch = new EnumSwitch<Colors>()
+                .Case(new[] { Colors.Red, Colors.Blue }, () => Console.WriteLine("Red or Blue"))
+                .Case(Colors.Green, () => Console.WriteLine("Green"));
+
+            eSwitch.Execute(Colors.Red);
+            eSwitch.Execute(Colors.Blue);
+        }
+
+        [Test]
+        public void Ignore()
+        {
+            new EnumSwitch<Colors>(Colors.Red)
+                .Case(Colors.Blue, () => Console.WriteLine("Blue"))
+                .Ignore(Colors.Red, Colors.Green)
+                .Execute();
+        }
+
+        [Test]
+        public void SwitchOnNotProvided()
+        {
+            Exception ex = Assert.Throws<InvalidOperationException>(
+                () =>
+                new EnumSwitch<Colors>().Execute()
+                );
+            Assert.AreEqual("SwitchOn not provided, either provide SwitchOn in constructor or use Execute(switchOn)", ex.Message);
+        }
+
+        [Test]
+        public void InvalidTypeArgument()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(
+                () =>
+                new EnumSwitch<int>(10).Execute(10)
+                );
+            Assert.AreEqual("Type argument System.Int32 must be an enum", ex.Message);
+        }
+
+        [Test]
+        public void DuplicateCaseLabel()
+        {
+            Exception ex = Assert.Throws(
+                typeof(ArgumentException),
+                () => new EnumSwitch<Colors>(Colors.Red)
+                          .Case(Colors.Blue, () => Console.WriteLine("Blue"))
+                          .Case(Colors.Blue, () => Console.WriteLine("Blue Again"))
+                          .Case(Colors.Green, () => Console.WriteLine("Green"))
+                          .Execute());
+
+            Assert.AreEqual("Duplicate case label Blue\r\nParameter name: option", ex.Message);
+        }
+
+        [Test]
+        public void UnhandledCase()
+        {
+            Exception ex = Assert.Throws(
+                typeof(NotImplementedException),
+                () => new EnumSwitch<Colors>(Colors.Red)
+                          .Case(Colors.Blue, () => Console.WriteLine("Blue"))
+                          .Case(Colors.Green, () => Console.WriteLine("Green"))
+                          .Execute());
+
+            Assert.AreEqual(ex.Message, "Switch statement didn't handle cases: Red");
+        }
+    }
+}
