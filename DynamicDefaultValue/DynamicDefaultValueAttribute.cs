647c5ad088a490ee1c42ac82ba9ca9ba1eaffcb0
Faisal Mansoor <faisal.mansoor@gmail.com>
2013-07-05 15:26:52 -0400

    Added DynamicDefaultValue

---
+++ DynamicDefaultValue/DynamicDefaultValueAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+
+namespace DynamicDefaultValue
+{
+    public class DynamicDefaultValueAttribute : DefaultValueAttribute
+    {
+        static DynamicDefaultValueAttribute()
+        {
+            try
+            {
+                foreach (DictionaryEntry v in Environment.GetEnvironmentVariables())
+                {
+                    DictionaryEntry v1 = v;
+                    AddOrUpdate(v.Key.ToString(), key => v1.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to initialize DynamicDefaultValueAttribute class. {0}", ex.Message);
+            }
+        }
+
+        private static readonly ConcurrentDictionary<string, Func<string, object>> KeyValueMap =
+            new ConcurrentDictionary<string, Func<string, object>>();
+
+        public DynamicDefaultValueAttribute(string key)
+            : base(null)
+        {
+            try
+            {
+                Func<string, object> mapper;
+                if (KeyValueMap.TryGetValue(key, out mapper))
+                {
+                    object mapped = mapper(key);
+                    SetValue(mapped);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to inititalize DynamicDefaultValueAttribute instance for key: {0}. {1}", key, ex);
+            }
+        }
+
+        public static void AddOrUpdate(string key, Func<string, object> mapper)
+        {
+            KeyValueMap.AddOrUpdate(key, mapper, (k, v) => mapper);
+        }
+
+        public static void AddOrUpdate(string key, object value)
+        {
+            KeyValueMap.AddOrUpdate(key, k => (k2 => value), (k, v) => (k2 => value));
+        }
+
+        public static void Remove(string key)
+        {
+            Func<string, object> mapper;
+            KeyValueMap.TryRemove(key, out mapper);
+        }
+    }
+}
---
+++ DynamicDefaultValue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DynamicDefaultValue
+{
+    public class Configuration
+    {
+        [DefaultValue(@"C:\Database")]
+        public string DatabasePath;
+
+        [DynamicDefaultValue("TEMP_PATH")]
+        public string TempPath;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicDefaultValueAttribute.AddOrUpdate("TEMP_PATH", Path.GetTempPath());
+
+            var serializer = new XmlSerializer(typeof(Configuration));
+            var config = new Configuration { DatabasePath = @"C:\Database", TempPath = Path.GetTempPath() };
+
+            // Should not print any thing. All values are set to default.
+            serializer.Serialize(Console.Out, config);
+
+
+            Console.Write("\n\n");
+
+            config.TempPath = @"C:\Temp";
+            serializer.Serialize(Console.Out, config);
+        }
+    }
+}
